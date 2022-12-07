cbefbaf2de9e72b155ee54ab01710ce78653406d
Oleksandra <sashkaburko1994@gmail.com>
2022-12-07 03:15:57 +0200

    add lazy and suspense

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,34 @@
 import { Routes, Route } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
 
-import HomePage from 'pages/HomePage/HomePage';
-import MovieDetailPage from 'pages/MovieDetailPage/MovieDetailPage';
-import MoviesPage from '../../pages/MoviesPage/MoviesPage';
-import CastPage from '../CastPage/CastPage';
-import ReviewesPage from '../ReviewesPage/ReviewesPage';
-import Layout from '../../Layout';
 import Header from '../Header/Header';
 import { Wrapper } from './App.styled';
 
+const Layout = lazy(() => import('../../Layout/Layout'));
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage.jsx'));
+const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage.jsx'));
+const MovieDetailPage = lazy(() =>
+  import('../../pages/MovieDetailPage/MovieDetailPage.jsx')
+);
+const CastPage = lazy(() => import('../CastPage/CastPage.jsx'));
+const ReviewesPage = lazy(() => import('../ReviewesPage/ReviewesPage.jsx'));
+
 export const App = () => {
   return (
-    <Layout>
-      <Header />
-      <Wrapper>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId/*" element={<MovieDetailPage />}>
-            <Route path="cast" element={<CastPage />} />
-            <Route path="reviews" element={<ReviewesPage />} />
-          </Route>
-        </Routes>
-      </Wrapper>
-    </Layout>
+    <Suspense>
+      <Layout>
+        <Header />
+        <Wrapper>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/movies/:movieId/*" element={<MovieDetailPage />}>
+              <Route path="cast" element={<CastPage />} />
+              <Route path="reviews" element={<ReviewesPage />} />
+            </Route>
+          </Routes>
+        </Wrapper>
+      </Layout>
+    </Suspense>
   );
 };
